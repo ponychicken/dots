839d0c661daad1076974931c65cf578df32f06e8
Leo Koppelkamm <hello@leo-koppelkamm.de>
2014-12-04 14:22:47 +0100

    Update index.sh

--- os/osx/index.sh
+++ os/osx/index.sh
@@ -19,11 +19,3 @@ sh "$osx/defaults.sh"
 sh "$osx/binaries.sh"
 sh "$osx/apps.sh"
 
-# Symlink the profile
-if [[ ! -e "$HOME/.bash_profile" ]]; then
-  echo "symlinking: $osx/profile.sh => $HOME/.bash_profile"
-  symlink "$osx/profile.sh" "$HOME/.bash_profile"
-  source $HOME/.bash_profile
-else
-  echo "$HOME/.bash_profile already exists. remove and run again."
-fi
